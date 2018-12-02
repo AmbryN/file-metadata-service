532ea5e12bada0da5369b60740e454137c0ba0b1
Nicolas AMBRY <nicolas.ambry@gmail.com>
2018-12-02 15:05:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# File Metadata service
+
+Sends back a JSON object of an uploaded file's metadata.
+
+It returns :
+- name
+- type
+- size in bytes
+
+
+## Getting Started
+
+* Clone the repository
+* Go to localhost:8080 and upload a file
+
+
+## Built With
+
+* [Node.js](https://nodejs.org/) - Server
+* [Express.js](https://expressjs.com/) - Web Framework
+
+
+## Authors
+
+* **Nicolas AMBRY** - *Initial work* - [AmbryN](https://github.com/AmbryN)
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const fileUpload = require('express-fileupload')
+
+const app = express()
+app.use(fileUpload())
+
+// Serve the form in the web directory
+app.get('/', express.static('web'))
+
+// Upload a file to this route
+app.post('/api/file', (req, res, next) => {
+    // If no file was uploaded
+    if (Object.keys(req.files).length ==0) {
+        return res.status(400).send("No files were uploaded")
+    }
+
+    // Get the file from the request
+    const upfile = req.files.upfile
+
+    // Send back file metadata as JSON
+    res.json({name: upfile.name, type: upfile.mimetype, size: upfile.data.length})
+})
+
+app.listen(8080, console.log("App listening on port 8080"))
